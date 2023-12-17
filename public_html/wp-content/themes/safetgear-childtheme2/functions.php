6108d0da4db9a79e6a81f1e623e81e3a7061ab9e
dk <deepan.k@blackboard.com>
2023-12-17 23:23:14 +0530

    latest fix

--- public_html/wp-content/themes/safetgear-childtheme2/functions.php
+++ public_html/wp-content/themes/safetgear-childtheme2/functions.php
@@ -5,7 +5,13 @@ function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
 
-function wpb_custom_new_menu() {
-  register_nav_menu('pr-po-menu',__( 'PR PO Menu' ));
-}
+// function wpb_custom_new_menu() {
+//   register_nav_menu('pr-po-menu',__( 'PR PO Menu0' ));
+// }
 
+add_action('woocommerce_thankyou', 'custom_code_after_place_order');
+
+function custom_code_after_place_order($order_id) {
+    // Your custom code goes here
+    echo '<p>Your custom content after placing the order</p>';
+}
